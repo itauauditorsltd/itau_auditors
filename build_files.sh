47d00ab71053f0418006ef65081945f45555085d
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-16 16:12:09 +0200

    build_files updated

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
-# build_files.sh
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt
+
+# Ensure pip is installed
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python3.9 manage.py migrate
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
