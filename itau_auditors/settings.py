24cb631dc5ba9631e473ed75457eac6eb1439b66
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-18 16:44:43 +0200

    postgresql

--- itau_auditors/settings.py
+++ itau_auditors/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = "itau_auditors.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway', 
+        'USER': 'postgres',
+        'PASSWORD': 'PdFIRvvLVAcXZNhxOqBeCfSEqkgTLgXW',
+        'HOST': 'monorail.proxy.rlwy.net', 
+        'PORT': '48849',
     }
 }
+
 
 
 # Password validation
