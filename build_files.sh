8ab85e65f4650dd5cb38ec7c8756621098b5cb25
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-19 16:59:20 +0200

    aws s3 buildfiles

--- build_files.sh
+++ build_files.sh
@@ -4,21 +4,15 @@
 set -e
 
 # Ensure pip is installed
-echo "Ensuring pip is installed..."
 python3.9 -m ensurepip --upgrade
-echo "Upgrading pip..."
 python3.9 -m pip install --upgrade pip
 
 # Install dependencies
-echo "Installing dependencies..."
 pip install -r requirements.txt
 
 # Make and apply migrations
-echo "Making migrations..."
 python3.9 manage.py makemigrations
-echo "Applying migrations..."
 python3.9 manage.py migrate
 
 # Collect static files
-echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
