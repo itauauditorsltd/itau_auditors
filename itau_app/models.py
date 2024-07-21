18b882c64ceaf385cd7287025fcb9f8929cfc670
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-21 17:51:42 +0200

    storage

--- itau_app/models.py
+++ itau_app/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.core.files.storage import default_storage
 # Create your models here.
 from django.utils import timezone
 
 class CompanyInfo(models.Model):
-    logo = models.ImageField(upload_to='logos/')
-    logo_two = models.ImageField(upload_to='logostwo/')
+    logo = models.ImageField(upload_to='logos/', storage=default_storage)
+    logo_two = models.ImageField(upload_to='logostwo/', storage=default_storage)
     street_address = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
-    working_hours = models.CharField(max_length=100,  null=True, blank=True)
-    email = models.CharField(max_length=100,  null=True, blank=True)
-    brochure = models.FileField(upload_to='brochures/', null=True, blank=True)
+    working_hours = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    brochure = models.FileField(upload_to='brochures/', null=True, blank=True, storage=default_storage)
     
     facebook = models.URLField(max_length=200, blank=True, null=True)
     twitter = models.URLField(max_length=200, blank=True, null=True)
@@ -24,7 +24,7 @@ class CompanyInfo(models.Model):
 class AboutUs(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='about_us_images/')
+    image = models.ImageField(upload_to='about_us_images/', null=True, blank=True, storage=default_storage)
 
     def __str__(self):
         return self.title
@@ -42,14 +42,14 @@ class Testimony(models.Model):
     description = models.TextField(null=True, blank=True)
     client_name = models.CharField(max_length=50, null=True, blank=True, db_index=True)
     job_title = models.CharField(max_length=25, null=True, blank=True)
-    image = models.ImageField(upload_to='testimonials/', null=True, blank=True)
+    image = models.ImageField(upload_to='testimonials/', null=True, blank=True, storage=default_storage)
 
     def __str__(self):
         return self.client_name
     
 class Partner(models.Model):
     name = models.CharField(max_length=200,  null=True, blank=True)
-    logo = models.ImageField(upload_to='partners/', null=True, blank=True)
+    logo = models.ImageField(upload_to='partners/', null=True, blank=True, storage=default_storage)
 
     def __str__(self):
         return self.name
@@ -62,13 +62,13 @@ class Tag(models.Model):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='blog_images/')
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=default_storage)
     author = models.CharField(max_length=100, blank=True, null=True)
     date = models.DateField(default=timezone.now)
     description = models.TextField()  # Content of the blog
     category = models.CharField(max_length=100)
-    subImage = models.ImageField(upload_to='blog_images/', blank=True, null=True)
-    subImage2 = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    subImage = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=default_storage)
+    subImage2 = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=default_storage)
     subTitle = models.CharField(max_length=200, blank=True, null=True)
     subDescription = models.TextField(blank=True, null=True)
     likes = models.IntegerField(default=0)
@@ -83,7 +83,7 @@ class BlogPost(models.Model):
 
 class OurTeam(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
-    image = models.ImageField(upload_to='ourTeam_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='ourTeam_images/', null=True, blank=True, storage=default_storage)
     position = models.CharField(max_length=100)
     facebook = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
--- itau_auditors/settings.py
+++ itau_auditors/settings.py
@@ -94,3 +94,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
