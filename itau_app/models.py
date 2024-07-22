f955188d9756ec2255d49add623b7f6ef033e680
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-22 14:35:01 +0200

    updates models set

--- itau_app/models.py
+++ itau_app/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.utils import timezone
-from storages.backends.s3boto3 import S3Boto3Storage
 
 class CompanyInfo(models.Model):
-    logo = models.ImageField(upload_to='logos/', storage=S3Boto3Storage())
-    logo_two = models.ImageField(upload_to='logostwo/', storage=S3Boto3Storage())
+    logo = models.ImageField(upload_to='logos/')
+    logo_two = models.ImageField(upload_to='logostwo/')
     street_address = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
     working_hours = models.CharField(max_length=100, null=True, blank=True)
     email = models.CharField(max_length=100, null=True, blank=True)
-    brochure = models.FileField(upload_to='brochures/', null=True, blank=True, storage=S3Boto3Storage())
+    brochure = models.FileField(upload_to='brochures/', null=True, blank=True)
     
     facebook = models.URLField(max_length=200, blank=True, null=True)
     twitter = models.URLField(max_length=200, blank=True, null=True)
@@ -23,7 +22,7 @@ class CompanyInfo(models.Model):
 class AboutUs(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='about_us_images/', null=True, blank=True, storage=S3Boto3Storage())
+    image = models.ImageField(upload_to='about_us_images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -40,14 +39,14 @@ class Testimony(models.Model):
     description = models.TextField(null=True, blank=True)
     client_name = models.CharField(max_length=50, null=True, blank=True, db_index=True)
     job_title = models.CharField(max_length=25, null=True, blank=True)
-    image = models.ImageField(upload_to='testimonials/', null=True, blank=True, storage=S3Boto3Storage())
+    image = models.ImageField(upload_to='testimonials/', null=True, blank=True)
 
     def __str__(self):
         return self.client_name
 
 class Partner(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
-    logo = models.ImageField(upload_to='partners/', null=True, blank=True, storage=S3Boto3Storage())
+    logo = models.ImageField(upload_to='partners/', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -60,17 +59,17 @@ class Tag(models.Model):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=S3Boto3Storage())
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     author = models.CharField(max_length=100, blank=True, null=True)
     date = models.DateField(default=timezone.now)
-    description = models.TextField()
+    description = models.TextField()  # Content of the blog
     category = models.CharField(max_length=100)
-    subImage = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=S3Boto3Storage())
-    subImage2 = models.ImageField(upload_to='blog_images/', null=True, blank=True, storage=S3Boto3Storage())
+    subImage = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    subImage2 = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     subTitle = models.CharField(max_length=200, blank=True, null=True)
     subDescription = models.TextField(blank=True, null=True)
     likes = models.IntegerField(default=0)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)  # Many-to-Many relationship with Tag
 
     def __str__(self):
         return self.title
@@ -80,7 +79,7 @@ class BlogPost(models.Model):
 
 class OurTeam(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
-    image = models.ImageField(upload_to='ourTeam_images/', null=True, blank=True, storage=S3Boto3Storage())
+    image = models.ImageField(upload_to='ourTeam_images/', null=True, blank=True)
     position = models.CharField(max_length=100)
     facebook = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
