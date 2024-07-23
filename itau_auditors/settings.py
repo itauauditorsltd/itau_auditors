7c83904b94ccb50371419439dffd455693b3ecc8
itauauditorsltd <itauauditorsltd@gmail.com>
2024-07-23 09:00:51 +0200

    vercel env valiables

--- itau_auditors/settings.py
+++ itau_auditors/settings.py
@@ -2,6 +2,11 @@ import os
 from pathlib import Path
 import boto3
 from botocore.client import Config
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,16 +57,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "itau_auditors.wsgi.application"
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'PdFIRvvLVAcXZNhxOqBeCfSEqkgTLgXW',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '48849',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
@@ -76,10 +92,10 @@ USE_I18N = True
 USE_TZ = True
 
 # AWS S3 settings
-AWS_ACCESS_KEY_ID = 'AKIAXYKJRUQ736NI7Z4P'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'itauauditors3'
-AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_SIGNATURE_VERSION = 's3v4'
@@ -92,5 +108,9 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
